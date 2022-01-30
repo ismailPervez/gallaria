7b6886abb93d1ac9f4d5347b5d8180b04fe7a7d9
ismailPervez <freelanceismail001@gmail.com>
2022-01-30 12:09:59 +0300

    created a register view / form

--- ruheena/users/views.py
+++ ruheena/users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'users/index.html')
 
 def login(request):
-    return render(request, 'users/login.html')
+    return render(request, 'users/login.html', {'title': 'login to your personal gallery - Ruheena'})
 
 def register(request):
-    return HttpResponse("register page")
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
