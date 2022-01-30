d1d9405b4e3c9e35b84ce0a3bc9b76220156e625
ismailPervez <freelanceismail001@gmail.com>
2022-01-30 22:24:28 +0300

    added a Post model

--- ruheena/users/models.py
+++ ruheena/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    picture = models.ImageField(upload_to='gallery/')
+    caption = models.CharField(max_length=220)
+    date_posted = models.DateField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- ruheena/users/urls.py
+++ ruheena/users/urls.py
@@ -2,10 +2,12 @@ from unicodedata import name
 from django.urls import path
 from . import views as users_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', users_views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register', users_views.register, name='register')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
